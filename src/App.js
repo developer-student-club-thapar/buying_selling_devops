03509a9126ccbf865e6be7400cee0e38af20b678
Aniket Biswas <abiswas_be19@thapar.edu>
2020-05-01 15:01:56 +0530

    Initial setup of frontend with private routes done.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import Routes from "./routes";
+import history from "./services/history";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Routes />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
