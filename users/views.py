9542ce64fa0e9b4b880508afad09d2d7892314a8
Saurav <sauravsrivastav103@gmail.com>
2020-05-08 20:00:13 +0530

    OAuth Setup with Google account

--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
 from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 import requests
+from django.conf import settings
+
+
+User = settings.AUTH_USER_MODEL
 
 
 class GoogleView(APIView):
@@ -14,3 +21,18 @@ class GoogleView(APIView):
             content = {'message': 'wrong google token / this google token is already expired.'}
             return Response(content)
         return Response(r)
+
+        try:
+            user = User.objects.get(email=data['email'])
+        except User.DoesNotExist:
+            user = User()
+            user.password = make_password(BaseUserManager().make_random_password())
+            user.email = data['email']
+            user.save()
+
+        token = RefreshToken.for_user(user)
+        response = {}
+        response['username'] = user.username
+        response['access_token'] = str(token.access_token)
+        response['refresh_token'] = str(token)
+        return Response(response)
