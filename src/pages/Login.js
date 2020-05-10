15074a7ede8d97b022226051819fb7576370f3db
Aniket Biswas <abiswas_be19@thapar.edu>
2020-05-10 22:47:22 +0530

    Login page added

---
+++ src/pages/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import LoginBG from '../assets/login_bg.jpg';
+import { Row, Col, Divider } from 'antd';
+import { Typography } from 'antd';
+import styles from '../styles/Login.module.css';
+import { Button } from 'antd';
+import { GoogleOutlined } from '@ant-design/icons';
+
+const Login = () => {
+  const { Title } = Typography;
+  return (
+    <div className={styles.main}>
+      <Row
+        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+        className={styles.heading}
+      >
+        <Col span={24}>
+          <Title level={1}>ThaparX</Title>
+        </Col>
+        <Col span={24}>
+          <Title level={3} className={styles.subHeading}>
+            Buying and Selling made easy
+          </Title>
+        </Col>
+        <Col span={24}>
+          <Button
+            type="primary"
+            className={styles.button}
+            icon={<GoogleOutlined />}
+          >
+            {' '}
+            <Title
+              level={3}
+              style={{ margin: 'auto', display: 'inline-block' }}
+            >
+              Log in with Google
+            </Title>
+          </Button>
+        </Col>
+      </Row>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}></Row>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,14 @@ import { Switch } from 'react-router-dom';
 import test from '../pages/test';
 import PrivateTest from '../pages/PrivateTest';
 import Route from './Route';
+import Login from '../pages/Login';
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={test} />
+      <Route exact path="/" component={Login} />
       <Route exact path="/private" component={PrivateTest} isPrivate />
-      <Route component={test} />
+      <Route component={Login} />
     </Switch>
   );
 };
