dd68687b688b1145d2b7dfbd58dd725ba4a715f8
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-05-23 18:22:21 +0530

    Remove jwt configuration Tried to merge everything in one

--- buying_selling/settings/base.py
+++ buying_selling/settings/base.py
@@ -1,6 +1,6 @@
 import os
 import environ
-from .jwt_settings import JWT_CONFIG
+from datetime import timedelta
 
 env = environ.Env()
 
@@ -96,3 +96,24 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # JWT SETTINGS
 SIMPLE_JWT = JWT_CONFIG
+
+SIGNING_KEY=env(
+    "SIGNING_KEY",
+    default="",
+)
+
+JWT_CONFIG = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SIGNING_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
