c65a52e52574c065c76980c570ddfc4b943c0fde
Jay Parmani <jsparmani@gmail.com>
2020-05-23 14:06:18 +0530

    update jwt configs

--- buying_selling/settings/jwt_settings.py
+++ buying_selling/settings/jwt_settings.py
@@ -8,13 +8,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
+if env('SIGNING_KEY'):
+    SIGNING_KEY = env('SIGNING_KEY')
+else:
+    SIGNING_KEY = ""
+
 JWT_CONFIG = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY': env('SIGNING_KEY'),
+    'SIGNING_KEY': SIGNING_KEY,
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
