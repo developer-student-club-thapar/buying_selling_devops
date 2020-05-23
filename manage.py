a318cfc50f3d88e42ff2069c8e259642e69e13d8
parthsujalshah <parthsujalshah@gmail.com>
2020-05-23 19:07:02 +0530

    added an environment check for DJANGO_SETTINGS_MODULE

--- manage.py
+++ manage.py
@@ -2,10 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import environ
+
+env = environ.Env()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+
+DJANGO_ENV = env('DJANGO_ENV')
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buying_selling.settings")
+    if DJANGO_ENV == "PRODUCTION":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buying_selling.settings.prod")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buying_selling.settings.dev")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
