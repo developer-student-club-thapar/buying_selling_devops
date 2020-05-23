ca4c03f5f32a7c985126dff1da6ec85f9fbf89b3
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2020-05-23 18:26:41 +0530

    Removed dev settings Trying to merge everything in one file

--- buying_selling/settings/base.py
+++ buying_selling/settings/base.py
@@ -32,6 +32,13 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
 ]
 
+if DEBUG:
+    STATIC_URL = "/static/"
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
+    STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 SITE_ID = 1
 
 MIDDLEWARE = [
