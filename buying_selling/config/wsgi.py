ee85a0b0ae72c6ccd57e9c33bb68e4679e660a90
Jay Parmani <jsparmani@gmail.com>
2020-06-10 00:46:56 +0530

    change wsgi config

--- buying_selling/config/wsgi.py
+++ buying_selling/config/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buying_selling.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buying_selling.config.settings")
 
 application = get_wsgi_application()
