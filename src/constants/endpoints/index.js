745dd10be932ddbe491e89cf78cd7851b8cda715
Jay Parmani <jsparmani@gmail.com>
2020-06-11 00:12:59 +0530

    update backend endpoint

--- src/constants/endpoints/index.js
+++ src/constants/endpoints/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_ENDPOINT = 'http://127.0.0.1:8000';
+export const BACKEND_ENDPOINT = 'https://thaparolx.dsctiet.tech';
 export const AUTH_ENDPOINT = `${BACKEND_ENDPOINT}/google/auth/token/`;
 export const POST_ENDPOINT = `${BACKEND_ENDPOINT}/api/posts/`;
 export const PROFILE_ENDPOINT = `${BACKEND_ENDPOINT}/api/users/profile/`;
