d0da335751cfd75465ffb4917f42ae1388d85985
Jay Parmani <jsparmani@gmail.com>
2020-06-10 19:29:40 +0530

    remove static file dir setting

--- buying_selling/config/settings.py
+++ buying_selling/config/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
 
 if DEBUG:
     STATIC_URL = "/static/"
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
     STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
