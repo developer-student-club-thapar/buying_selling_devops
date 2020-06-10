d1542c6630865f62d40a085e55756c8742fdefa3
Jay Parmani <jsparmani@gmail.com>
2020-06-10 19:14:57 +0530

    change dev static dir path

--- buying_selling/config/settings.py
+++ buying_selling/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
 if DEBUG:
     STATIC_URL = "/static/"
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
-    STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 else:
