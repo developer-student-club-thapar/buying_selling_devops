80bd8a30fc84613e8a84f4d3061d9061f932c34c
specter25 <ujjwalcoding012@gmail.com>
2020-12-12 16:10:21 +0530

    single docker file for the server and client complete

--- deploy/build_local.sh
+++ deploy/build_local.sh
@@ -1,10 +1,12 @@
-yarn build
+#!/usr/bin/env sh
+
+# yarn build
 
 mkdir -p build/root
 for file in $(ls build | grep -E -v '^(index\.html|static|root)$'); do
     mv "build/$file" build/root;
 done
 
-poetry shell
+pipenv shell
 
 python ./manage.py collectstatic --no-input
