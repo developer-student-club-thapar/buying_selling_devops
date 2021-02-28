2b5e9a9f3ca8d9aae2334b4e6b1c204718efe29e
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-02-28 13:35:25 +0530

    Refresh token now gets stored in a cookie

--- buying_selling/users/views.py
+++ buying_selling/users/views.py
@@ -5,6 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import exceptions
+import jwt
+from django.conf import settings
 import requests
 from .models import MyUser
 
@@ -17,6 +20,9 @@ class HelloView(APIView):
         return Response({"message": "hi"})
 
 
+# - Use the following APIs while generating access token on Google OAuth playground -
+#       https://www.googleapis.com/auth/userinfo.email
+#       https://www.googleapis.com/auth/userinfo.profile
 class GoogleView(APIView):
     def post(self, request):
         payload = {"access_token": request.data.get("token")}  # validate the token
@@ -45,8 +51,42 @@ class GoogleView(APIView):
                 return Response({"error": "invalid email"})
 
         token = RefreshToken.for_user(user)
-        response = {}
+
+        response = Response()
         response["username"] = user.username
         response["access_token"] = str(token.access_token)
-        response["refresh_token"] = str(token)
-        return Response(response)
+
+        # set the refresh token as a http-only cookie
+        response.set_cookie(key='refreshtoken', value=str(token), httponly=True)
+
+        return response
+
+
+class RefreshTokenView(APIView):
+    """
+    Used to generate a new access token from the
+    refresh token stored in the http only cookie
+    """
+
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refreshtoken')
+        if refresh_token is None:
+            raise exceptions.AuthenticationFailed('Authentication credentials were not provided.')
+        try:
+            payload = jwt.decode(refresh_token, settings.SIGNING_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('expired refresh token, please login again.')
+
+        user = MyUser.objects.get(id=payload.get('user_id'))
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed('user is inactive')
+
+        token = RefreshToken.for_user(user)
+        response = Response()
+        response["username"] = user.username
+        response["access_token"] = str(token.access_token)
+
+        return response
