b97ccf489ea636c1c5c51f3ff4e5abbed0393ae7
Mahima <kulsmahima@gmail.com>
2022-02-01 22:47:51 +0530

    changed user actions and reducers to hooks

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -3,50 +3,41 @@ import {
   PROFILE_ENDPOINT,
   MY_PROFILE_ENDPOINT,
 } from '../../constants/endpoints/index';
-import {createAction} from '@reduxjs/toolkit'
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createAction } from '@reduxjs/toolkit';
+import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 
 //Get my profile
 export const getMyProfile = (token) => async (dispatch) => {
   try {
     const res = await fetchBaseQuery({
-      baseUrl : `${MY_PROFILE_ENDPOINT}`,
-      prepareHeaders : (headers) =>{
-          headers.set('Authorization', `Bearer ${token}`)
-          return headers
-      }
-    })
-    const action = createAction(GET_MYPROFILE)
-    dispatch(
-      action(res.data)
-      );
-    } catch (err) {
-      const action = createAction(PROFILE_ERROR)
-      dispatch(
-        action(err.response.data)
-      );
-      }
-    };  
+      baseUrl: `${MY_PROFILE_ENDPOINT}`,
+      prepareHeaders: (headers) => {
+        headers.set('Authorization', `Bearer ${token}`);
+        return headers;
+      },
+    });
+    const action = createAction(GET_MYPROFILE);
+    dispatch(action(res.data));
+  } catch (err) {
+    const action = createAction(PROFILE_ERROR);
+    dispatch(action(err.response.data));
+  }
+};
 
 //Get profile by id
 export const getProfile = (id, token) => async (dispatch) => {
   try {
     const res = await fetchBaseQuery({
-      baseUrl : `${PROFILE_ENDPOINT}${id}`,
-      prepareHeaders : (headers) =>{
-          headers.set('Authorization', `Bearer ${token}`)
-          return headers
-      }
-    })
-    const action = createAction(GET_PROFILE)
-    dispatch(
-      action(res.data)
-    );
+      baseUrl: `${PROFILE_ENDPOINT}${id}`,
+      prepareHeaders: (headers) => {
+        headers.set('Authorization', `Bearer ${token}`);
+        return headers;
+      },
+    });
+    const action = createAction(GET_PROFILE);
+    dispatch(action(res.data));
   } catch (err) {
-    const action = createAction(PROFILE_ERROR)
-    dispatch(
-      action(err.response.data)
-    );
+    const action = createAction(PROFILE_ERROR);
+    dispatch(action(err.response.data));
   }
-}
- 
+};
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,5 +1,5 @@
 import { GET_MYPROFILE, GET_PROFILE, PROFILE_ERROR } from '../types';
-import {createReducer} from '@reduxjs/toolkit'
+import { createReducer } from '@reduxjs/toolkit';
 
 const defaultState = {
   myProfile: null,
@@ -7,27 +7,27 @@ const defaultState = {
   error: null,
 };
 
-export default createReducer(defaultState, (builder)=>{
+export default createReducer(defaultState, (builder) => {
   builder
-    .addCase(GET_MYPROFILE, (state,action)=>{
-      return  {
+    .addCase(GET_MYPROFILE, (state, action) => {
+      return {
         ...state,
         myProfile: action.payload,
       };
     })
-    .addCase(GET_PROFILE, (state, action)=>{
+    .addCase(GET_PROFILE, (state, action) => {
       return {
         ...state,
         profile: action.payload,
       };
     })
-    .addCase(PROFILE_ERROR,(state, action)=>{
+    .addCase(PROFILE_ERROR, (state, action) => {
       return {
         ...state,
         error: action.payload,
       };
     })
-    .addDefaultCase((state, action)=>{
-      return state
-    })
-})
+    .addDefaultCase((state, action) => {
+      return state;
+    });
+});
