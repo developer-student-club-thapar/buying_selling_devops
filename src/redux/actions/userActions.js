264bd39e338177d91590e68390a44f48cca93250
Mahima <kulsmahima@gmail.com>
2022-02-03 12:50:02 +0530

    fixed hooks

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,21 +1,20 @@
 import { GET_MYPROFILE, GET_PROFILE, PROFILE_ERROR } from '../types';
+import axios from 'axios';
 import {
   PROFILE_ENDPOINT,
   MY_PROFILE_ENDPOINT,
 } from '../../constants/endpoints/index';
 import { createAction } from '@reduxjs/toolkit';
-import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 
 //Get my profile
 export const getMyProfile = (token) => async (dispatch) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
   try {
-    const res = await fetchBaseQuery({
-      baseUrl: `${MY_PROFILE_ENDPOINT}`,
-      prepareHeaders: (headers) => {
-        headers.set('Authorization', `Bearer ${token}`);
-        return headers;
-      },
-    });
+    const res = await axios.get(`${MY_PROFILE_ENDPOINT}`, config);
     const action = createAction(GET_MYPROFILE);
     dispatch(action(res.data));
   } catch (err) {
@@ -26,14 +25,13 @@ export const getMyProfile = (token) => async (dispatch) => {
 
 //Get profile by id
 export const getProfile = (id, token) => async (dispatch) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
   try {
-    const res = await fetchBaseQuery({
-      baseUrl: `${PROFILE_ENDPOINT}${id}`,
-      prepareHeaders: (headers) => {
-        headers.set('Authorization', `Bearer ${token}`);
-        return headers;
-      },
-    });
+    const res = await axios.get(`${PROFILE_ENDPOINT}${id}`, config);
     const action = createAction(GET_PROFILE);
     dispatch(action(res.data));
   } catch (err) {
