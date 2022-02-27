bcb1e718e7765af0ff6a583f31c24244486afc72
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-02-27 10:38:26 +0530

    adding pagination to posts

--- buying_selling/config/settings.py
+++ buying_selling/config/settings.py
@@ -134,6 +134,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    # just for checking purpose, will increase this to 5 or 10
+    "PAGE_SIZE": 1,
 }
 
 # CORS SETTINGS
